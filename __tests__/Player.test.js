33213d24e6b311cec1f6134ae309be01e452ea45
DJMIKE20 <m.zimm20@gmail.com>
2022-09-12 17:00:24 -0400

    testing and build players health methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,8 +1,9 @@
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
+
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
+// console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -34,4 +35,36 @@ test('creates a player object', () => {
     player.inventory = [];
   
     expect(player.getInventory()).toEqual(false);
+  });
+
+  // test to get information about the player's health
+  test("gets player's health value", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+  });
+
+  //checking if player is alive
+  test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+  
+    expect(player.isAlive()).toBeTruthy();
+  
+    player.health = 0;
+  
+    expect(player.isAlive()).toBeFalsy();
+  });
+
+  // too see if the correct amount of health is being subtracted from the Player health property
+  test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
   });
